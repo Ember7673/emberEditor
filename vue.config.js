4fc6faef72c259ff61f757b018029289ce722de2
汪腾腾 <1751727782@qq.com>
2020-11-18 11:56:54 +0800

    修改删除代码时，控制台报错问题

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 /*
  * @Author: wangtengteng
  * @Date: 2020-10-30 18:13:15
- * @LastEditTime: 2020-11-18 10:23:59
+ * @LastEditTime: 2020-11-18 10:25:50
  * @FillPath: Do not edit
  */
 const path = require('path');
 module.exports = {
-  pages: {
-    index: {
-      entry: 'src/lib/index.js'
-    }
-  },
+  // pages: {
+  //   index: {
+  //     entry: 'src/lib/index.js'
+  //   }
+  // },
   /* 输出文件目录：在npm run build时，生成文件的目录名称 */
   outputDir: 'dist',
   /* 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录 */
